0b731ad40bcff242626fb4d6270140d001991678
CallMeLaNN <git@lann.me>
2022-05-31 16:33:58 +0800

    getItemsService overloads for filter and action

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,16 +2,50 @@ import { ItemsService } from "directus"
 import type { Request } from "express"
 
 import type ExtensionContext from "../types/ExtensionContext"
+import type { FilterHookContext } from "../types/RegisterHook/FilterHookHandler"
+import type { ActionHookContext } from "../types/RegisterHook/ActionHookHandler"
 import type Models from "../models"
 
 export function getItemsService<
   TItem extends Models[TCollectionName],
   TCollectionName extends keyof Models,
->(collectionName: TCollectionName, req: Request, context: ExtensionContext) {
+>(
+  collectionName: TCollectionName,
+  context: FilterHookContext,
+): ItemsService<TItem>
+
+export function getItemsService<
+  TItem extends Models[TCollectionName],
+  TCollectionName extends keyof Models,
+>(
+  collectionName: TCollectionName,
+  context: ActionHookContext,
+): ItemsService<TItem>
+
+export function getItemsService<
+  TItem extends Models[TCollectionName],
+  TCollectionName extends keyof Models,
+>(
+  collectionName: TCollectionName,
+  req: Request,
+  context: ExtensionContext,
+): ItemsService<TItem>
+
+export function getItemsService<
+  TItem extends Models[TCollectionName],
+  TCollectionName extends keyof Models,
+>(
+  collectionName: TCollectionName,
+  reqOrFilterCtxOrActionCtx: Request | FilterHookContext | ActionHookContext,
+  context?: ExtensionContext,
+): ItemsService<TItem> {
   const itemsService = new ItemsService<TItem>(collectionName, {
-    schema: req.schema,
-    accountability: req.accountability,
-    knex: context.database,
+    schema: reqOrFilterCtxOrActionCtx.schema,
+    accountability: reqOrFilterCtxOrActionCtx.accountability,
+    knex:
+      "database" in reqOrFilterCtxOrActionCtx
+        ? reqOrFilterCtxOrActionCtx.database
+        : context?.database,
   })
   return itemsService
 }
